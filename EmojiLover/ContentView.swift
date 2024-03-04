afa68c3ae6b602a592bbe65efa653f83efb321f6
ikhsansyahrizal <ikhsansyahrizal23@gmail.com>
2024-03-04 22:37:44 +0700

    initial commit

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Ikhsan on 04/03/24.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 🐙, 🚀, 😭, 🤡, 🤓
+}
+
+struct ContentView: View {
+    
+    @State var selection: Emoji = .🐙
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self){ emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers App")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Ikhsan on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
